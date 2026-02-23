9952391b97d50801abe1d2f22a86217e975534de
keza-laura <k.laura@alustudent.com>
2026-02-23 08:36:46 -0800

    api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,32 +12,28 @@ def top_ten(subreddit):
     Prints the titles of the first 10 hot posts.
     If the subreddit is invalid, prints None.
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://api.reddit.com/r/{}/hot".format(subreddit)
 
     headers = {
-        "User-Agent": "alu-reddit-top-ten-script/1.0"
+        "User-Agent": "alu-reddit-script/1.0"
     }
 
     params = {
         "limit": 10
     }
 
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False
-        )
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
 
-        if response.status_code != 200:
-            print(None)
-            return
-
-        data = response.json().get("data", {}).get("children", [])
-
-        for post in data:
-            print(post.get("data", {}).get("title"))
-
-    except Exception:
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+
+    for post in data:
+        print(post.get("data", {}).get("title"))
